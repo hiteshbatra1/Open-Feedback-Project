78219ffe368d34ef0de2db47bd4b1fe59c889466
hiteshbatra1 <hiteshbatra967@gmail.com>
2026-02-12 19:33:48 +0530

    feat: add email verification setup using Resend

---
+++ src/app/api/sign-up/route.ts
@@ -0,0 +1,23 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import bcrypt from "bcryptjs";
+import { sendVerificationEmail } from "@/helper/sendVerificationEmail";
+
+export async function POST(request: Request) {
+  await dbConnect();
+
+  try {
+    const { username, email, password } = await request.json();
+  } catch (error) {
+    console.error("Error in registring user", error);
+    return Response.json(
+      {
+        success: false,
+        message: "Error in registring user",
+      },
+      {
+        status: 500,
+      },
+    );
+  }
+}
