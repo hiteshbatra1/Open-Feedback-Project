70126f215cc46dac9ea16b10259c4f65bb99ad81
hiteshbatra1 <hiteshbatra967@gmail.com>
2026-02-15 16:25:36 +0530

    feat: add send message API and fetch messages using aggregation pipeline

---
+++ src/app/api/get-messages/route.ts
@@ -0,0 +1,63 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/options";
+import { User } from "next-auth";
+import mongoose from "mongoose";
+
+export async function POST(request: Request) {
+  await dbConnect();
+
+  const session = await getServerSession(authOptions);
+  const user: User = session?.user;
+
+  if (!session || !session.user) {
+    return Response.json(
+      {
+        success: false,
+        message: "Not Autheticated",
+      },
+      { status: 401 },
+    );
+  }
+  // Convert the user ID to a Mongoose ObjectId for aggregation pipeline.
+  //  It can create issue because in session we store it as string but in database it is stored as ObjectId.
+  //  So we need to convert it before using it in aggregation pipeline
+  const userId = new mongoose.Types.ObjectId(user._id);
+
+  try {
+    const user = await UserModel.aggregate([
+      { $match: { _id: userId } },
+      { $unwind: "$messages" },
+      { $sort: { "messages.createdAt": -1 } },
+      { $group: { _id: "$_id", messages: { $push: "$messages" } } },
+    ]);
+
+    if (!user || user.length === 0) {
+      return Response.json(
+        {
+          success: false,
+          message: "User not found",
+        },
+        { status: 404 },
+      );
+    }
+
+    return Response.json(
+      {
+        success: true,
+        messages: user[0].messages,
+      },
+      { status: 200 },
+    );
+  } catch (error) {
+    console.error("Internal Server error", error);
+    return Response.json(
+      {
+        success: false,
+        message: "Internal server error",
+      },
+      { status: 500 },
+    );
+  }
+}
