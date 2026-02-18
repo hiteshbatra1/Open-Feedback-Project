8893e33d7743f35a0e90a2b2f108eef05f8b1649
hiteshbatra1 <hiteshbatra967@gmail.com>
2026-02-18 14:26:27 +0530

    feat: add API to delete user messages

---
+++ src/app/api/delete-message/[messageid]/route.ts
@@ -0,0 +1,61 @@
+import UserModel from "@/model/User";
+import dbConnect from "@/lib/dbConnect";
+import { authOptions } from "../../auth/[...nextauth]/options";
+import { getServerSession } from "next-auth";
+import { User } from "next-auth";
+
+export async function Delete(
+  request: Request,
+  { params }: { params: { messageid: string } },
+) {
+  const messageId = params.messageid;
+
+  await dbConnect();
+
+  const session = await getServerSession(authOptions);
+  const user: User = session?.user;
+
+  if (!session || !session.user) {
+    return Response.json(
+      {
+        success: false,
+        message: "Not authenticated",
+      },
+      { status: 401 },
+    );
+  }
+  try {
+    const updateResult = await UserModel.updateOne(
+      { _id: user._id },
+      { $pull: { messages: { _id: messageId } } },
+    );
+
+    if (updateResult.modifiedCount === 0) {
+      return Response.json(
+        {
+          success: false,
+          message: "Message not found or already deleted",
+        },
+        { status: 404 },
+      );
+    }
+
+    return Response.json(
+      {
+        success: true,
+        message: "Message deleted",
+      },
+      { status: 200 },
+    );
+  } catch (error) {
+    console.error("Internal Server Error");
+
+    return Response.json(
+      {
+        success: false,
+        message: "Internal server errror",
+      },
+      { status: 500 },
+    );
+  }
+}
