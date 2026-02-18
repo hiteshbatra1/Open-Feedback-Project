8afdf36b1e27b58af1d82cc355b4869b18a6edf2
hiteshbatra1 <hiteshbatra967@gmail.com>
2026-02-18 21:08:26 +0530

    feat: design home page with carousel functionality

---
+++ src/app/(app)/page.tsx
@@ -0,0 +1,69 @@
+"use client";
+import React from "react";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import Autoplay from "embla-carousel-autoplay";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import messages from "@/messages.json";
+import { Mail } from "lucide-react";
+import Link from "next/link";
+
+const Home = () => {
+  return (
+    <>
+      <main className="flex flex-col items-center justify-center px-4 md:px-24 py-12 bg-gray-800 text-white">
+        <section className="text-center mb-8 md:mb-12">
+          <h1 className="text-3xl md:text-5xl font-bold">
+            Dive into the World of Anonymous Feedback
+          </h1>
+          <p className="mt-3 md:mt-4 text-base md:text-lg">
+            Open Feedback - Where your identity remains a secret.
+          </p>
+        </section>
+        <Carousel
+          plugins={[Autoplay({ delay: 2000 })]}
+          className="w-full max-w-lg md:max-w-xl"
+        >
+          <CarouselContent>
+            {messages.map((message, index) => (
+              <CarouselItem key={index} className="p-4">
+                <Card>
+                  <CardHeader>
+                    <CardTitle>{message.title}</CardTitle>
+                  </CardHeader>
+                  <CardContent className="flex flex-col md:flex-row items-start space-y-2 md:space-y-0 md:space-x-4">
+                    <Mail className="flex-wrap" />
+                    <div>
+                      <p>{message.content}</p>
+                      <p className="text-xs text-muted-foreground">
+                        {message.received}
+                      </p>
+                    </div>
+                  </CardContent>
+                </Card>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+      </main>
+      <footer className="text-center p-4 md:p-6 bg-gray-900 text-white">
+        © 2026 Open Feedback. Created by{" "}
+        <span className="text-blue-200">
+          <Link href="https://www.linkedin.com/in/hitesh-batra-h">
+            Hitesh Batra
+          </Link>
+        </span>
+        . All rights reserved.
+      </footer>
+    </>
+  );
+};
+
+export default Home;
