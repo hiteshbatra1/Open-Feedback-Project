2cfb18a3455d8b8974ba17c18dcdfb630c56d62b
hiteshbatra1 <hiteshbatra967@gmail.com>
2026-04-12 13:11:05 +0530

    enhancement: update username validation to allow only lowercase letters and numbers

--- src/schemas/signUpSchema.ts
+++ src/schemas/signUpSchema.ts
@@ -4,7 +4,10 @@ export const usernameValidation = z
   .string()
   .min(2, "Username must be atleast 2 chracters")
   .max(20, "Username must be not more that 20 chracters")
-  .regex(/^[a-zA-Z0-9]+$/, "Username must not contain special chracters");
+  .regex(
+    /^[a-z0-9]+$/,
+    "Username must contain only lowercase letters and numbers",
+  );
 
 export const signUpSchema = z.object({
   username: usernameValidation,
